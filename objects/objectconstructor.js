65e630f82f83caa9a7e4068034746f95727ec1dc
atwanzireT <tatwanzire@gmail.com>
2022-08-24 01:37:16 +0300

    constructors

---
+++ objects/objectconstructor.js
@@ -0,0 +1,13 @@
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+    this.nationality = "English";
+}
+
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+
+console.log(myFather)
+console.log(myMother)
