5b3a030afa10df6401ee09b37b7a6412e8c7b5b7
atwanzireT <tatwanzire@gmail.com>
2022-08-24 01:56:29 +0300

    async

---
+++ async/callbacks.js
@@ -0,0 +1,13 @@
+function myDisplayer(text){
+    console.log(text)
+}
+function myFirst() {
+    myDisplayer("Hello");
+}
+  
+function mySecond() {
+    myDisplayer("Goodbye");
+}
+  
+myFirst();
+mySecond();
